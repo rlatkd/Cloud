d0756425825a9376eb6184bad6469f0d9ac2361e
rlatkdgns <tkdgns96@naver.com>
2023-11-27 19:28:14 +0900

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,45 @@
+# 프로바이더
 provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-086cae3329a3f7d75"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserversg.id]
-  user_data              = <<-EOF
-    #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p ${var.service_port} &
-  EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
-  }
+# iam.tf을 참조하여 사용자 생성
+module "iam_users" {
+  source = "./module/iam.tf"
 }
 
-resource "aws_security_group" "webserversg" {
-  name = "terraform-example-webserversg"
-
-  ingress {
-    from_port   = var.service_port
-    to_port     = var.service_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_iam_user" "rlatkdReact" {
+  name = module.iam_users.aws_iam_user.rlatkdReact.name
 }
 
-variable "service_port" {
-  description = "Service Port for HTTP Request"
-  type        = number
-  default     = 8080
+resource "aws_iam_user" "rlatkdFlask" {
+  name = module.iam_users.aws_iam_user.rlatkdFlask.name
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "Public IP for Web Server"
+resource "aws_iam_user" "rlatkdMySQL" {
+  name = module.iam_users.aws_iam_user.rlatkdMySQL.name
 }
 
-output "service_url" {
-  value       = "http://${aws_instance.example.public_ip}:${var.service_port}"
-  description = "Web Server Service URL"
+
+# vpc.tf를 참조하여 vpc 생성
+module "custom_vpc" {
+  source = "./module/vpc.tf"
+}
+
+
+# ec2.tf를 참조하여 EC2 생성
+module "ec2_instance" {
+  source = "./module/ec2.tf"
+}
+
+
+# s3React.tf를 참조하여 s3 생성
+module "s3" {
+  source = "./module/s3React.tf"
+}
+
+
+# rds.tf를 참조하여 rds 생성
+module "rds" {
+  source = "./module/rds.tf"
 }
